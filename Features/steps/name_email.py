4f4e6071a5f7c56bb56b5df62d46e53818295f5f
Anantha <basayana@gmail.com>
2026-06-18 18:22:09 +0530

    added notes to run with print statement

--- Features/steps/name_email.py
+++ Features/steps/name_email.py
@@ -10,6 +10,6 @@ logger = logging.getLogger(__name__)
 def fill_name_email(context, name, email):
     name_email_page = name_email(context.driver)
     name_email_page.enter_name_email(name, email)
-    print(f"%%%%%%%%%%%%%Entered name: {name}, email: {email}", flush=True)
+    print(f"%%%%%%%%%%%%%Entered name: {name}, email: {email}")
     logger.info(f"******************")
     assert 1==1
